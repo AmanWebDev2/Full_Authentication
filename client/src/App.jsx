adc8971dd674eb6b7febd83b11609ba2b6bebcfc
Aman <webdevaman2@gmail.com>
2023-03-25 16:14:05 +0530

    setup tailwind and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react"
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+/** import all components */
+import PageNotFound from './components/PageNotFound';
+import Username from './components/Username';
+import Password from './components/Password';
+import Register from './components/Register';
+import Recovery from './components/Recovery';
+import Profile from './components/Profile';
+import Reset from './components/Reset';
+
+
+/** root routes */
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element:<Username />
+  },
+  {
+    path:'/register',
+    element:<Register/>
+  },
+  {
+    path:'/profile',
+    element:<Profile/>
+  },
+  {
+    path:'/recovery',
+    element:<Recovery/>
+  },
+  {
+    path:'/reset',
+    element:<Reset/>
+  },
+  {
+    path:'/password',
+    element:<Password />
+  },
+  {
+    path:'*',
+    element:<PageNotFound/>
+  },
+])
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <h1>Rect app</h1>
-    </div>
+    <main>
+        <RouterProvider router={router}></RouterProvider>
+    </main>
   )
 }
 
