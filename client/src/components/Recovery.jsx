ce9dbd320164ba3730b9792496807f489cd1252e
Aman <webdevaman2@gmail.com>
2023-03-25 18:06:16 +0530

    recovery ui done

--- client/src/components/Recovery.jsx
+++ client/src/components/Recovery.jsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
+import { useFormik } from 'formik';
+import { passwordValidate } from '../helper/validate';
+import avatar from '../assets/profile.png';
+
+import styles from '../styles/Username.module.css';
 
 const Recovery = () => {
+   
   return (
-    <div>Recovery</div>
+    <>
+    <div className="container mx-auto">
+
+    {/* <Toaster position='top-center' reverseOrder={false}></Toaster> */}
+
+      <div className='flex justify-center items-center h-screen'>
+      <div className={styles.glass}>
+
+        <div className="title flex flex-col items-center">
+          <h4 className='text-5xl font-bold'>Recovery</h4>
+          <span className='py-4 text-xl w-2/3 text-center text-gray-500'>
+            Enter OTP to recover password
+          </span>
+        </div>
+
+        <form className='py-2'>
+           
+          <div className="textbox flex flex-col items-center gap-6">
+              <div className="py-4 text-sm text-left text-gray-500">
+                Enter 6 digit OTP sent o your email
+              </div>
+              <input type="text" placeholder='OTP' className={styles.textbox}/>
+              <button  className={styles.btn} type="submit">Recover</button>
+          </div>
+
+          <div className="text-center py-4">
+            <span className='text-gray-500'>Can't get OTP &nbsp;
+            <button className='text-red-500' >Resend Now</button></span>
+          </div>
+        </form>
+
+
+      </div>
+      </div>
+    </div>
+    </>
   )
 }
 
-export default Recovery
+export default Recovery;
